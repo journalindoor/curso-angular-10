23fb46732452e72a5dca951eb80fee46e88875b8
RSMJr <journal.indoor@gmail.com>
2020-09-27 14:18:56 -0300

    44 - Configurando as rotas de navegação no componente principal

--- projetos/gerenciador-de-tarefas/src/app/app.module.ts
+++ projetos/gerenciador-de-tarefas/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
