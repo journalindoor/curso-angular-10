7b822047ee517b39ffb52ae7b846fbe72f4841f1
RSMJr <journal.indoor@gmail.com>
2020-09-27 03:07:39 -0300

    28 - Criando o Componente Calculadora

--- projetos/calculadora/src/app/app.module.ts
+++ projetos/calculadora/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
